15685998fb0884f521776cbf5f51eba00f8e1b68
Andrew Hao <andrewhao@gmail.com>
2016-07-11 23:10:41 -0700

    Adding extended opt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', (process.env.PORT || 5000));
 
