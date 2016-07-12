0cfcb8cb54a9643291a3c6a8716100746b37da35
Andrew Hao <andrewhao@gmail.com>
2016-07-11 23:03:19 -0700

    Fix typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.urlEncoded());
+app.use(bodyParser.urlencoded());
 
 app.set('port', (process.env.PORT || 5000));
 
