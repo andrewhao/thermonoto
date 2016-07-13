00bd68d5f38d65640d8f270713a47bf26f93abe5
Andrew Hao <andrew@carbonfive.com>
2016-07-13 09:24:10 -0700

    Adding Keen

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var keenIO = require('keen.io');
 var app = express();
+
+keen = keenIO.configure({
+  projectId: process.env.KEEN_PROJECT_ID,
+  writeKey: process.env.KEEN_WRITE_KEY
+});
 
 app.use(bodyParser.raw());
 
@@ -9,11 +15,18 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/temperature_updates', function(request, response) {
   response.sendStatus(200);
 });
+
 app.post('/temperature_updates', function(request, response) {
-  console.log(request.query)
-  console.log(request.body)
-  console.log(request)
-  response.sendStatus(200);
+  console.log(request.body);
+  keen.addEvent("temperature_updates", { temperature: request.body.temperature }, function(err, res) {
+    if(err) {
+      console.error("Error updating Keen", err);
+      throw "Error updating Keen.";
+    }
+    else {
+      response.sendStatus(200);
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
