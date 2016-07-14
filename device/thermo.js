c3fac6ddc8e1a1e5a038c58a6bf744a947b9e2a0
Andrew Hao <andrew@carbonfive.com>
2016-07-14 16:38:51 -0700

    Rename to wifiController. Refactor some syntax changes.

--- device/thermo.js
+++ device/thermo.js
@@ -1,15 +1,15 @@
 var tessel = require('tessel');
-var wifiManager = require('./wifi');
+var wifiManager = require('./wifiManager');
 var wifi = require('wifi-cc3000');
 var gpio = tessel.port.GPIO;
 var request = require('request');
 var toggleLed = require('./toggleLed');
 
 function readTemperatureAndReport() {
-  console.log('reading temp')
+  console.log('reading temp');
   var pin = gpio.analog[0];
-  console.log(pin)
-  var rawPinValue = pin.read()
+  console.log(pin);
+  var rawPinValue = pin.read();
   var volts = rawPinValue * 3300.0 / pin.resolution;
   console.log('volts', volts);
   var temp_C = (volts - 0.5) * 100.0;
@@ -24,38 +24,42 @@ function readTemperatureAndReport() {
 	toggleLed(0, 2);
   request.post({url: url, form: data}, function(e, res) {
     if (!e) {
-			console.log('success', res.body)
+			console.log('success', res.body);
 			toggleLed(1, 2);
 		}
-    else { console.error("http error", e) }
+    else { console.error("http error", e); }
 	});
 }
 
 wifi.on("connect", function(data) {
-	setInterval(readTemperatureAndReport, 10000)
+	setInterval(readTemperatureAndReport, 10000);
 	toggleLed(0, 100);
   console.log('wifi> on:connect', data);
 });
+
 wifi.on("disconnect", function(data) {
-	clearInterval(readTemperatureAndReport)
+	clearInterval(readTemperatureAndReport);
 	toggleLed(1, 100);
   console.log('wifi> on:disconnect', data);
 });
+
 wifi.on("timeout", function(data) {
   console.log('wifi> on:timeout', data);
-  wifiManager.powerCycle()
+  wifiManager.powerCycle();
 });
+
 wifi.on("error", function(data) {
   console.log('wifi> on:error', data);
 });
 
-console.log("Running thermo.js...")
+console.log("Running thermo.js...");
+
 setTimeout(function() {
-	console.log("[wifiManager] Connecting with the wifiManager..")
+	console.log("[wifiManager] Connecting with the wifiManager..");
   wifiManager.connect(function(err, res) {
 		if(!err) {
 			console.log('[wifiManager] connected', res);
 		}
-    else { console.error('[wifiManager] connection error', err) }
+    else { console.error('[wifiManager] connection error', err); }
   })
 }, 6000);
--- device/wifiManager.js
+++ device/wifiManager.js
@@ -4,33 +4,36 @@ var toggleLed = require('./toggleLed');
 
 var wifiManager = {
   connect: function connect(next) {
-    console.log("[wifiManager] Connecting to %s...", config.wifi.network)
-		console.log(config.wifi)
-    // Green blink
-    toggleLed(0, 4, 1000)
+    console.log("[wifiManager] Connecting to %s...", config.wifi.network);
+		console.log(config.wifi);
 
-    wifi.connect({
-      security: config.wifi.security || 'wpa2'
-      , ssid: config.wifi.network
-      , password: config.wifi.password
+    // Green blink
+    toggleLed(0, 4, 1000);
+
+		wifi.connect({
+			security: config.wifi.security,
+			ssid: config.wifi.network,
+			password: config.wifi.password
     }, next);
-  }
-  ,
+  },
   powerCycle: function powerCycle() {
     wifi.reset(function() {
+
       // Blue blink
       toggleLed(1, 4, 1000);
       console.log('[wifiManager] Reset called');
+
       var tryAgain = function() {
         if (!wifi.isConnected()) {
           console.log('[wifiManager] Wifi not connected, trying...');
-          wifiManager.connect()
+          wifiManager.connect();
         }
       };
 
       setTimeout(tryAgain, 20 * 1000);
-    })
+    });
   }
 };
+
 console.log("[wifiManager] Exporting...");
 module.exports = wifiManager;
