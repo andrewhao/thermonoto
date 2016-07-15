5398be2435eef92fdf1550efa978bbb2c314b394
Andrew Hao <andrewhao@gmail.com>
2016-07-14 23:12:46 -0700

    More tweaks

--- device/thermo.js
+++ device/thermo.js
@@ -4,64 +4,75 @@ var request = require('request');
 var led = require('tessel-led');
 var tesselWifi = require('tessel-wifi');
 var config = require("./config.json");
+request.debug = true;
 
 function readTemperatureAndReport() {
-  console.log('reading temp');
+  console.log('[readTemperatureAndReport]');
   var pin = gpio.analog[0];
   console.log(pin);
   var rawPinValue = pin.read();
   var volts = rawPinValue * 3300.0 / pin.resolution;
-  console.log('volts', volts);
+  console.log('[readTemperatureAndReport] volts', volts);
   var temp_C = (volts - 0.5) * 100.0;
   var temp_F = (temp_C * 9.0 / 5.0) + 32;
-  console.log('celsius:', temp_C);
-  console.log('fahrehneit:', temp_F);
+  console.log('[readTemperatureAndReport] celsius:', temp_C);
+  console.log('[readTemperatureAndReport] fahrehneit:', temp_F);
 
+  postToKeen(temp_F);
+}
+
+function postToKeen(temp_F) {
   var url = 'http://thermonoto.herokuapp.com/temperature_updates';
   var data = {temperature: temp_F};
 
-  console.log("POSTing to", url, data);
-  led.blue.flash(5, 50);
+  request.get({url: url}, function(e, res) { console.log('made get', e, res); });
+
+  console.log("[postToKeen] POSTing to", url, data);
   request.post({url: url, form: data}, function(e, res) {
+    console.log('[postToKeen] response', e, res);
     if (!e) {
-			console.log('success', res.body);
-      led.blue.flash(10, 25);
+			console.log('[postToKeen] success', res.body);
 		}
     else {
-      led.red.flash(5, 50);
-      console.error("http error", e);
+      console.error("[postToKeen] http error", e);
     }
 	});
 }
 
-function handleWifiError(err) {
-  console.error(err);
-  wifi.reconnect();
-  led.red.blink();
-}
-
 tesselWifi.prototype.DEBUG = true;
 
-var wifi = new tesselWifi({
-  ssid: config.wifi.network,
-  password: config.wifi.password,
-  reconnect: 10,
-  powerCycle: 5,
-});
-console.log('booting with wifi config', config.wifi)
-console.log(wifi);
+function main() {
+  function handleWifiError(err) {
+    console.error(err);
+    wifi.reconnect();
+    led.red.blink();
+  }
 
-wifi.on("connect", function(data) {
-  console.log('wifi> on:connect', data);
-  setInterval(readTemperatureAndReport, 10000);
-  led.green.show();
-})
-.on("disconnect", function(data) {
-  console.log('wifi> on:disconnect', data);
-  led.green.hide();
-  clearInterval(readTemperatureAndReport);
-})
-.on('error', handleWifiError);
+  var wifi = new tesselWifi({
+    ssid: config.wifi.network,
+    password: config.wifi.password,
+    reconnect: 10,
+    powerCycle: 5,
+  });
+  console.log('booting with wifi config', config.wifi)
+  console.log(wifi);
 
-console.log("Running thermo.js...");
+  wifi.on("connect", function(data) {
+    console.log('wifi> on:connect', data);
+    setInterval(readTemperatureAndReport, 10000);
+    led.green.show();
+    led.blue.hide();
+  })
+  .on("disconnect", function(data) {
+    console.log('wifi> on:disconnect', data);
+    led.green.hide();
+    led.blue.show();
+    clearInterval(readTemperatureAndReport);
+  })
+  .on('error', handleWifiError);
 
+  console.log("Running thermo.js...");
+  led.blue.show();
+}
+
+setTimeout(main, 6000);
