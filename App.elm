52486116124b0ad711c9a6878534c2c2642acd39
Andrew Hao <andrewhao@gmail.com>
2017-03-20 23:27:03 -0700

    Yay, got some JSON decoding working

--- App.elm
+++ App.elm
@@ -1,132 +1,87 @@
-{- This file re-implements the Elm Counter example (1 counter) with elm-mdl
-   buttons. Use this as a starting point for using elm-mdl components in your own
-   app.
--}
-
-
-module Main exposing (..)
-
 import Html exposing (..)
-import Html.Attributes exposing (href, class, style)
-import Material
-import Material.Scheme
-import Material.Button as Button
-import Material.Options exposing (css)
-
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (..)
+import Debug exposing (..)
 
 -- MODEL
-
-
--- You have to add a field to your model where you track the `Material.Model`.
--- This is referred to as the "model container"
 type alias Model =
-    { count : Int
-    , mdl :
-        Material.Model
-        -- Boilerplate: model store for any and all Mdl components you use.
-    }
+  { startTime : String
+  , endTime : String
+  }
 
+init : (Model, Cmd Msg)
+init =
+  ( Model "" ""
+  , getOperatingHoursDetails
+  )
 
--- `Material.model` provides the initial model
-model : Model
-model =
-    { count = 0
-    , mdl =
-        Material.model
-        -- Boilerplate: Always use this initial Mdl model store.
-    }
-
-
-
--- ACTION, UPDATE
-
-
--- You need to tag `Msg` that are coming from `Mdl` so you can dispatch them
--- appropriately.
+-- UPDATE
 type Msg
-    = Increase
-    | Reset
-    | Mdl (Material.Msg Msg)
+  = RefreshOperatingHours
+  | ReceiveOperatingHoursDetails (Result Http.Error JsonResponse)
 
-
-
--- Boilerplate: Msg clause for internal Mdl messages.
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        Increase ->
-            ( { model | count = model.count + 1 }
-            , Cmd.none
-            )
+  case msg of
+    RefreshOperatingHours ->
+      (model, getOperatingHoursDetails)
 
-        Reset ->
-            ( { model | count = 0 }
-            , Cmd.none
-            )
+    ReceiveOperatingHoursDetails (Ok jsonResponse) ->
+      Debug.log (jsonResponse |> toString)
+      ( Model
+         (Maybe.withDefault "" jsonResponse.start_time)
+         (Maybe.withDefault "" jsonResponse.end_time)
+      , Cmd.none)
 
-        -- When the `Mdl` messages come through, update appropriately.
-        Mdl msg ->
-            Material.update msg model
-
-
+    ReceiveOperatingHoursDetails (Err err) ->
+      Debug.log (toString err)
+      (model, Cmd.none)
 
 -- VIEW
-
-
-type alias Mdl =
-    Material.Model
-
-
 view : Model -> Html Msg
 view model =
-    div
-        [ style [ ( "padding", "2rem" ) ] ]
-        [ text ("Current count: " ++ toString model.count)
-          {- We construct the instances of the Button component that we need, one
-             for the increase button, one for the reset button. First, the increase
-             button. The first three arguments are:
-               - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-               - An instance id (the `[0]`). Every component that uses the same model
-                 collection (model.mdl in this file) must have a distinct instance id.
-               - A reference to the elm-mdl model collection (`model.mdl`).
-             Notice that we do not have to add fields for the increase and reset buttons
-             separately to our model; and we did not have to add to our update messages
-             to handle their internal events.
-             Mdl components are configured with `Options`, similar to `Html.Attributes`.
-             The `Button.onClick Increase` option instructs the button to send the `Increase`
-             message when clicked. The `css ...` option adds CSS styling to the button.
-             See `Material.Options` for details on options.
-          -}
-        , Button.render Mdl
-            [ 0 ]
-            model.mdl
-            [ Material.Options.onClick Increase
-            , css "margin" "0 24px"
-            ]
-            [ text "Increase" ]
-        , Button.render Mdl
-            [ 1 ]
-            model.mdl
-            [ Material.Options.onClick Reset ]
-            [ text "Reset" ]
-        ]
-        |> Material.Scheme.top
+  div []
+    [ h2 [] [text "Operating Hours"]
+    , button [ onClick RefreshOperatingHours ] [ text "Refresh" ]
+    , br [] []
+    , p [] [text (model.startTime |> toString)]
+    , p [] [text (model.endTime |> toString)]
+    ]
 
+-- SUBSCRIPTIONS
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
--- Load Google Mdl CSS. You'll likely want to do that not in code as we
--- do here, but rather in your master .html file. See the documentation
--- for the `Material` module for details.
+-- HTTP
 
+getOperatingHoursDetails : Cmd Msg
+getOperatingHoursDetails =
+  let
+    url =
+      "//localhost:5000/operating_hours"
+  in
+    Http.send ReceiveOperatingHoursDetails (Http.get url fetchOperatingHoursDetails)
 
--- main : Program Never
+type alias JsonResponse =
+  { start_time : Maybe String
+  , end_time : Maybe String
+  }
+
+fetchOperatingHoursDetails : Decoder JsonResponse
+fetchOperatingHoursDetails =
+  map2 JsonResponse
+    (field "start_time" (maybe string))
+    (field "end_time" (maybe string))
+
 main =
-    Html.program
-        { init = ( model, Cmd.none )
-        , view = view
-        -- Here we've added no subscriptions, but we'll need to use the `Mdl` subscriptions for some components later.
-        , subscriptions = always Sub.none
-        , update = update
-        }
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # thermonoto
+
+### Curl commands
+
+    $ curl -X PUT -H "Content-Type: application/x-www-form-urlencoded" -H "Cache-Control: no-cache" -H "Postman-Token: 7bf4777b-9958-9c56-1b27-92f2584ed7f9" -d 'start_time=5:00AM&end_time=6:00AM' "http://localhost:5000/operating_hours"
