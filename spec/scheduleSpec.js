4bea6aca092a6cdfb6adafb6a518ad61099084b6
Andrew Hao <andrewhao@gmail.com>
2017-03-27 23:35:27 -0700

    Adding mocha, chai

---
+++ spec/scheduleSpec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('Schedule', function() {
+  describe('#isOn', function() {
+    it("returns true", function() {
+      expect(true).toBe(true);
+    });
+  });
+});
