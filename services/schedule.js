2b1387fa44412efb00716a7868c062edb7126d38
Andrew Hao <andrew@carbonfive.com>
2017-03-31 18:07:35 -0700

    Add a scheduling service

---
+++ services/schedule.js
@@ -0,0 +1,27 @@
+function Schedule(startTime, endTime) {
+  this.startTime = new Date('2016-01-01 ' + startTime);
+  this.endTime = new Date('2016-01-01 ' + endTime);
+}
+
+Schedule.prototype.isOn = function(currentTime) {
+  if(currentTime === undefined) {
+    this.currentTime = new Date();
+  } else {
+    this.currentTime = currentTime;
+  }
+  console.log('currentTime', currentTime);
+  var hours = this.currentTime.getHours();
+
+  var startHours = this.startTime.getHours();
+  var endHours = this.endTime.getHours();
+
+  if(endHours < startHours) {
+    endHours += 24
+  }
+
+  console.log('startHours', startHours, 'endHours', endHours, 'currentHours', hours);
+
+  return hours >= startHours && hours <= endHours;
+}
+
+module.exports = Schedule;
